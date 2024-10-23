f0e7ddd06989c12371030f7f8f9c96df62534288
Alex Poputa <poputa.alex@gmail.com>
2024-10-23 21:24:12 +0300

    finished app and input conversion

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import { Component } from 'react';
+import { useEffect, useState } from 'react';
 import InputForm from './InputForm';
 import Search from './Search';
 import Contacts from './Contacts';
@@ -6,94 +6,77 @@ import { nanoid } from 'nanoid';
 import { Stack } from '@mui/material';
 import AlertBox from './AlertBox';
 
-export default class App extends Component {
+const App = () => {
+  const [contacts, setContacts] = useState([
+    { id: 0, name: 'Rosie Simpson', number: '459-12-56' },
+    { id: 1, name: 'Hermione Kline', number: '443-89-12' },
+    { id: 2, name: 'Eden Clements', number: '645-17-79' },
+    { id: 3, name: 'Annie Copeland', number: '227-91-26' },
+  ]);
 
+  const [filter, setFilter] = useState('');
+  const [openAlert, setOpenAlert] = useState(false);
 
-  componentDidMount(){
+  useEffect(() => {
     const data = localStorage.getItem('contacts');
     try {
-      if (data){
-        this.setState({
-          contacts: JSON.parse(data)
-        })
+      if (data) {
+        setContacts(JSON.parse(data));
       }
     } catch (error) {
       console.log(error);
     }
-  }
+  }, []);
 
-  componentDidUpdate( prevProps, prevState){
-    if(prevState?.contacts.length !== this.state.contacts.length){
-      localStorage.setItem('contacts', JSON.stringify(this.state.contacts))
-    }
-  }
-  
-  
-  state = {
-    contacts: [
-      { id: 0, name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 1, name: 'Hermione Kline', number: '443-89-12' },
-      { id: 2, name: 'Eden Clements', number: '645-17-79' },
-      { id: 3, name: 'Annie Copeland', number: '227-91-26' },
-    ],
-    filter: '',
-    isOpen: false,
+  useEffect(() => {
+    localStorage.setItem('contacts', JSON.stringify(contacts));
+  }, [contacts.length]);
+
+  const handleAlert = () => {
+    setOpenAlert(!openAlert);
   };
 
-  handleAlert = () => {
-    this.setState(prevState => ({ isOpen: !prevState.isOpen }));
-  };
-
-  handleSubmit = data => {
+  const handleSubmit = data => {
     const id = nanoid(10);
     const contactToAdd = {
       id: id,
       name: data.name,
       number: data.number,
     };
-    const found = this.state.contacts
+    const found = contacts
       .map(contact => contact.name.toLocaleLowerCase())
       .includes(data.name.toLocaleLowerCase());
     {
-      found
-        ? this.handleAlert()
-        : this.setState(prevState => {
-            return {
-              contacts: [...prevState.contacts, contactToAdd],
-            };
-          });
+      found ? handleAlert() : setContacts(...contacts, contactToAdd);
     }
   };
-  handleSearchChange = e => {
-    this.setState({ filter: e.target.value });
+  const handleSearchChange = e => {
+    setFilter(e.target.value);
   };
 
-  handleDelete = data => {
-    this.setState({ contacts: data });
+  const handleDelete = data => {
+    setContacts(data);
   };
 
-  render() {
-    const fileteredContacts = this.state.contacts.filter(contact => {
-      return contact.name
-        .toLocaleLowerCase()
-        .includes(this.state.filter.toLocaleLowerCase());
-    });
-    return (
-      <>
-        <Search onSearchChange={this.handleSearchChange} />
-        <Stack
-          direction='row'
-          width='100vw'
-          height='calc(100vh - 90px)'
-          backgroundColor='background.default'>
-          <InputForm onFormSubmit={this.handleSubmit} />
-          <Contacts contacts={fileteredContacts} onDelete={this.handleDelete}/>
-        </Stack>
-        <AlertBox
-          isOpen={this.state.isOpen}
-          onAlertToggle={this.handleAlert}
-        />
-      </>
-    );
-  }
-}
+  const fileteredContacts = contacts.filter(contact => {
+    return contact.name
+      .toLocaleLowerCase()
+      .includes(filter.toLocaleLowerCase());
+  });
+  return (
+    <>
+      <Search onSearchChange={handleSearchChange} />
+      <Stack
+        direction='row'
+        width='100vw'
+        height='calc(100vh - 90px)'
+        backgroundColor='background.default'>
+        <InputForm onFormSubmit={handleSubmit} />
+        <Contacts contacts={fileteredContacts} onDelete={handleDelete} />
+      </Stack>
+      <AlertBox isOpen={openAlert} onAlertToggle={handleAlert} />
+    </>
+  );
+};
+
+export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,13 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import App from './components/App';
 import { ThemeProvider } from '@emotion/react';
 import { theme } from './theme/theme';
+import  App  from './components/App';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <ThemeProvider theme={theme}>
-      <App />
+    <App/>
     </ThemeProvider>
   </StrictMode>
 );
